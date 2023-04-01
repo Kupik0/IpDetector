6069fab0004d7672102f2c4e6be62b76b8f48794
Mustafa Kupik <111908528+Kupik0@users.noreply.github.com>
2023-04-01 17:08:58 +0300

    Add project files.

---
+++ IpAdresiBulma/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        do
+        {
+            try
+            {
+                Console.WriteLine("Girmek İstediğiniz Url'yi giriniz:");
+                string Host = Console.ReadLine().ToString();
+
+                IPHostEntry hostname = Dns.GetHostByName(Host);
+
+                IPAddress[] ip = hostname.AddressList;
+
+                Console.WriteLine("İşlem başarılı ip: " + ip[0].ToString());
+
+                Console.WriteLine("\n");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("İşlem Başarısız Hata Kodu : {0}", ex.ToString);
+                Console.WriteLine("\n");
+            }
+
+
+        } while (true);
+
+    }
+
+}
